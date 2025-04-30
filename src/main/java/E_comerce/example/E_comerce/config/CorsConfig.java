273bd9bb7223551bd8824b14e26c1afb2158a508
Sanchit Thorat <sanchitcode77@gmail.com>
2025-04-30 18:18:37 +0530

    new added

--- src/main/java/E_comerce/example/E_comerce/config/CorsConfig.java
+++ src/main/java/E_comerce/example/E_comerce/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://shopie-ease.netlify.app/", "https://yourproductiondomain.com")
+                        .allowedOrigins("https://shopie-ease.netlify.app", "https://yourproductiondomain.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
